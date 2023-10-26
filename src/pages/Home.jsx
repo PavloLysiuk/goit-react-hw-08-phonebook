29715c7ef2020ec8fb614252ea47a6ab7f563f7d
Pavlo Lysiuk <121764119+PavloLysiuk@users.noreply.github.com>
2023-10-26 04:21:36 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,43 +6,41 @@ const { Heading, Center, Flex, Button, Box } = require('@chakra-ui/react');
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <Center>
-      <Flex m={0} p={0}>
-        <Box mx="auto" p="4" bg="#1a1d2b" borderRadius={8}>
-          <Center height="100%" flexDirection="column" p={16}>
-            <IoIosContacts size={80} color="#ffd76d" />
-            <Heading color="#ffffff" mb={12}>
-              PHONEBOOK
-            </Heading>
+    <>
+      <Box mx="auto" p="4" bg="#1a1d2b" borderRadius={8}>
+        <Center height="100%" flexDirection="column" p={16}>
+          <IoIosContacts size={80} color="#ffd76d" />
+          <Heading color="#ffffff" mb={12}>
+            PHONEBOOK
+          </Heading>
 
-            <Flex gap={2}>
-              <Button
-                type="button"
-                onClick={() => navigate('/login')}
-                variant="outline"
-                color="#ffd76d"
-                borderColor={'#ffd76d55'}
-                _hover={{ color: '#1a1d2b', bg: '#ffd76d' }}
-                _focus={{ color: '#1a1d2b', bg: '#ffd76d' }}
-              >
-                Login
-              </Button>
-              <Button
-                type="button"
-                onClick={() => navigate('/register')}
-                variant="outline"
-                color="#ffd76d"
-                borderColor={'#ffd76d55'}
-                _hover={{ color: '#1a1d2b', bg: '#ffd76d' }}
-                _focus={{ color: '#1a1d2b', bg: '#ffd76d' }}
-              >
-                Register
-              </Button>
-            </Flex>
-          </Center>
-        </Box>
-      </Flex>
-    </Center>
+          <Flex gap={2}>
+            <Button
+              type="button"
+              onClick={() => navigate('/login')}
+              variant="outline"
+              color="#ffd76d"
+              borderColor={'#ffd76d55'}
+              _hover={{ color: '#1a1d2b', bg: '#ffd76d' }}
+              _focus={{ color: '#1a1d2b', bg: '#ffd76d' }}
+            >
+              Login
+            </Button>
+            <Button
+              type="button"
+              onClick={() => navigate('/register')}
+              variant="outline"
+              color="#ffd76d"
+              borderColor={'#ffd76d55'}
+              _hover={{ color: '#1a1d2b', bg: '#ffd76d' }}
+              _focus={{ color: '#1a1d2b', bg: '#ffd76d' }}
+            >
+              Register
+            </Button>
+          </Flex>
+        </Center>
+      </Box>
+    </>
   );
 };
 
